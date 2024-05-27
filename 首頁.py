4346c1fc8fb003eab12010ab04194bebe06a44e6
wugliang <147639145+wugliang@users.noreply.github.com>
2024-05-27 20:42:07 +0800

    Add files via upload

---
+++ 首頁.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="政知道了",page_icon=":bar_chart:")
+
+st.title('政知道了')
+st.header('成大學生政治分析器')
+url = "https://www.facebook.com/gdscncku"
+st.write(f"[Designed and developed by GDSC BigData 2024]({url})")
+
