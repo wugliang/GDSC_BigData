7dc31094bfd70544a4323a6f8b2e91fd02f7bfe3
wugliang <147639145+wugliang@users.noreply.github.com>
2024-06-07 16:15:39 +0800

    Update D.數據.py

--- pages/D.數據.py
+++ pages/D.數據.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+st.set_page_config(page_title="政知道了",page_icon=":bar_chart:")
 dataset = pd.read_csv('datas.csv')
 
 dataset['us_relation_opinion'] = 1+(dataset['us_relation_opinion']-1)*1.5
